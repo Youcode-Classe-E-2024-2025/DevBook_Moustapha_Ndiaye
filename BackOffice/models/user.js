bdf651f3b1ca7659d1a16ea25045476da7f97e4e
amdymstfa <amdymoustapha011@gmail.com>
2025-04-25 00:12:46 +0100

    Feat: configure and implement user model

---
+++ BackOffice/models/user.js
@@ -0,0 +1,61 @@
+// models/user.js
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/database'); 
+const bcrypt = require('bcryptjs');
+
+class User extends Model {
+  async comparePassword(candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password);
+  }
+}
+
+User.init({
+  user_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false
+  },
+  name: {
+    type: DataTypes.STRING(100),
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+    unique: true,
+    validate: { 
+      isEmail: true,
+    }
+  },
+  password: {
+    type: DataTypes.STRING(255),
+    allowNull: false
+  },
+  role: {
+    type: DataTypes.ENUM('admin', 'member'),
+    allowNull: false,
+    defaultValue: 'member'
+  },
+}, {
+  sequelize,                
+  modelName: 'User',       
+  tableName: 'users',       
+  timestamps: true,        
+  hooks: {
+    beforeCreate: async (user) => {
+      if (user.password) {
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt);
+      }
+    },
+    beforeUpdate: async (user) => {
+      if (user.changed('password')) {
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt);
+      }
+    }
+  }
+});
+
+module.exports = User;
